e266ed41a46b0d95e75b7e0b958ec00b31e3488d
gallantsa <gallantsa@163.com>
2024-03-11 16:51:16 +0800

    学习LinkedList源码_构造函数_数据结构

--- JavaDS/src/main/java/Collection/List/LinkedList/_LinkedList.java
+++ JavaDS/src/main/java/Collection/List/LinkedList/_LinkedList.java
@@ -1,4 +1,15 @@
 package Collection.List.LinkedList;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class _LinkedList {
+    public static void main(String[] args) {
+        // 首尾指针维护的双向链表
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        // 构造函数可以直接传入Collection, ArrayList也是
+        LinkedList<Integer> integers = new LinkedList<>(new ArrayList<>(list));
+        System.out.println();
+    }
 }
