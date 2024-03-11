72b48af31ea1702b428a6f948aedf27016d3583b
gallantsa <gallantsa@163.com>
2024-03-11 16:45:24 +0800

    学习ArrayList源码_扩容

--- JavaDS/src/main/java/Collection/List/ArrayList/_ArrayList.java
+++ JavaDS/src/main/java/Collection/List/ArrayList/_ArrayList.java
@@ -1,11 +1,36 @@
 package Collection.List.ArrayList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class _ArrayList {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
+        // 底层是可变数组
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1); // 初始化时不指定容量, 默认为0, 当添加第一个元素时, 会扩为10, 之后再扩容都是1.5倍
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(1); // 每次扩容一倍
+        // Integer[] array = (Integer[]) list.toArray(); 报错
+//        Integer[] array = list.toArray(new Integer[0]);
+//        for (Integer integer : array) {
+//            System.out.println(integer);
+//        }
 
+
+
+        // toArray()要创建一个固定类型的字符串数组
+//        public <T > T[]toArray(T[]a){
+//            if (a.length < size)
+//                // Make a new array of a's runtime type, but my contents:
+//                return (T[]) Arrays.copyOf(elementData, size, a.getClass());
+//            System.arraycopy(elementData, 0, a, 0, size);
+//            if (a.length > size)
+//                a[size] = null;
+//            return a;
+//        }
     }
 }
