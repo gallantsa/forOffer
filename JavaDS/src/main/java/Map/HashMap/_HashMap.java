028ccefc3ab34aea4ee89235f83017bdc2187d0d
gallantsa <gallantsa@163.com>
2024-03-11 17:03:48 +0800

    学习HashMap和HashTable源码_浅尝

--- JavaDS/src/main/java/Map/HashMap/_HashMap.java
+++ JavaDS/src/main/java/Map/HashMap/_HashMap.java
@@ -1,4 +1,15 @@
 package Map.HashMap;
 
+import java.util.HashMap;
+
 public class _HashMap {
+    public static void main(String[] args) {
+        // static final float DEFAULT_LOAD_FACTOR = 0.75f; 负载因子默认0.75
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+        map.put(null, 1); // key为null的哈希值为0, 否则取hashCode再右移16位
+        // 键值都可以为空, key只能有一个为null, 值可以有多个
+        // 线程不安全, hashTable和ConcurrentHashMap都是线程安全的, ConcurrentHashMap更好
+    }
 }
--- JavaDS/src/main/java/Map/HashTable/_HashTable.java
+++ JavaDS/src/main/java/Map/HashTable/_HashTable.java
@@ -1,4 +1,17 @@
 package Map.HashTable;
 
+import java.util.Hashtable;
+
 public class _HashTable {
+    public static void main(String[] args) {
+        Hashtable<Integer, Integer> hashtable = new Hashtable<>();
+//        if (value == null) {
+//            throw new NullPointerException();
+//        }
+        // hashTable键值都不能为空, 空指针异常
+        // if (value == null) {
+        //            throw new NullPointerException();
+        //        }
+        // hashtable.put(null, null); // 报错
+    }
 }
