9d31b247e5d20c2de84ac02fc8ae115bc1f0ffd6
gallantsa <gallantsa@163.com>
2024-03-11 20:07:09 +0800

    了解ConcurrentHashMap

--- JavaDS/src/main/java/Map/ConcurrentHashMap/_ConcurrentHashMap.java
+++ JavaDS/src/main/java/Map/ConcurrentHashMap/_ConcurrentHashMap.java
@@ -1,4 +1,11 @@
 package Map.ConcurrentHashMap;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 public class _ConcurrentHashMap {
+    public static void main(String[] args) {
+        ConcurrentHashMap<Object, Object> hashMap = new ConcurrentHashMap<>();
+        // 简直不能为空, 空指针异常 if (key == null || value == null) throw new NullPointerException();
+        hashMap.put(null, null);
+    }
 }
