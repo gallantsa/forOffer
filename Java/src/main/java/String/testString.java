35ffe5702ab0fc5706615a6ed3c72a9d20d65fed
gallantsa <gallantsa@163.com>
2024-03-27 20:22:13 +0800

    探究字符串常量池

--- Java/src/main/java/String/testString.java
+++ Java/src/main/java/String/testString.java
@@ -16,5 +16,16 @@ public class testString {
 
 //        StringBuffer stringBuffer = new StringBuffer();
 //        stringBuffer.append("123"); // 也继承AbstractStringBuilder, 关键方法有synchronized
+        String str1 = "123";
+        String str2 = new String("123");
+        String str3 = str2;
+        System.out.println("str1 == str2：" + (str1 == str2)); // false
+        System.out.println("str1 == str3：" + (str1 == str3)); // false
+        System.out.println("str2 == str3：" + (str2 == str3)); // true
+
+        //通过java.lang.String.intern()方法指定字符串对象
+        String str4 = str2.intern();
+        System.out.println("str1 == str4：" + (str1 == str4)); // true
+        System.out.println("str2 == str4：" + (str2 == str4)); // false
     }
 }
