c4f114a232e23ea1ce5d8e5335a294fd74566e71
gallantsa <gallantsa@163.com>
2024-04-03 08:32:01 +0800

    实现布隆过滤器

---
+++ DS/src/main/java/布隆过滤器/Main.java
@@ -0,0 +1,16 @@
+package 布隆过滤器;
+
+public class Main {
+    public static void main(String[] args) {
+        String value1 = "https://javaguide.cn/";
+        String value2 = "https://github.com/Snailclimb";
+        MyBloomFilter filter = new MyBloomFilter();
+        System.out.println(filter.contains(value1));
+        System.out.println(filter.contains(value2));
+        filter.add(value1);
+        filter.add(value2);
+        System.out.println(filter.contains(value1));
+        System.out.println(filter.contains(value2));
+
+    }
+}
