b6d4d70ae31ca6c3eda856f605bd277bdbdd5421
gallantsa <gallantsa@163.com>
2024-08-30 18:59:36 +0800

    虚拟线程的简单使用

---
+++ OS/src/main/java/虚拟线程/VirtualThreadExecutorExample.java
@@ -0,0 +1,18 @@
+package 虚拟线程;
+
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+public class VirtualThreadExecutorExample {
+    public static void main(String[] args) {
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            IntStream.range(0, 10).forEach(i -> {
+                executor.submit(() -> {
+                    System.out.println("mianshiya.com " + i + " in virtual thread: " + Thread.currentThread());
+                });
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
