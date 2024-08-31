2473fde2a5efe5761ec691ffab2516e5e7a1b9fa
gallantsa <gallantsa@163.com>
2024-08-31 18:56:28 +0800

    当 Java 的 synchronized 升级到重量级锁时，会发生什么？

--- OS/src/main/java/虚拟线程/VirtualThreadExample.java
+++ OS/src/main/java/虚拟线程/VirtualThreadExample.java
@@ -1,17 +1,17 @@
 package 虚拟线程;
 
 public class VirtualThreadExample {
-    public static void main(String[] args) {
-        // 创建并启动一个虚拟线程
-        Thread virtualThread = Thread.ofVirtual().start(() -> {
-            System.out.println("mianshiya.com virtual thread: " + Thread.currentThread());
-        });
-
-        // 等待虚拟线程完成
-        try {
-            virtualThread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+//    public static void main(String[] args) {
+//        // 创建并启动一个虚拟线程
+//        Thread virtualThread = Thread.ofVirtual().start(() -> {
+//            System.out.println("mianshiya.com virtual thread: " + Thread.currentThread());
+//        });
+//
+//        // 等待虚拟线程完成
+//        try {
+//            virtualThread.join();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
--- OS/src/main/java/虚拟线程/VirtualThreadExecutorExample.java
+++ OS/src/main/java/虚拟线程/VirtualThreadExecutorExample.java
@@ -4,15 +4,15 @@ import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
 
 public class VirtualThreadExecutorExample {
-    public static void main(String[] args) {
-        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
-            IntStream.range(0, 10).forEach(i -> {
-                executor.submit(() -> {
-                    System.out.println("mianshiya.com " + i + " in virtual thread: " + Thread.currentThread());
-                });
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//    public static void main(String[] args) {
+//        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+//            IntStream.range(0, 10).forEach(i -> {
+//                executor.submit(() -> {
+//                    System.out.println("mianshiya.com " + i + " in virtual thread: " + Thread.currentThread());
+//                });
+//            });
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
